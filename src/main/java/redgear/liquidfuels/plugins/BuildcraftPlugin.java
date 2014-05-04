23de075ec96de028dd4cf85a3ed57c73f58fd83f
LordBlackhole <LordBlackhole@gmail.com>
2014-05-04 09:13:51 -0400

    Updated Buildcraft refinery recipes
    Now uses the new IMC commands in Buildcraft 6.0

--- src/main/java/redgear/liquidfuels/plugins/BuildcraftPlugin.java
+++ src/main/java/redgear/liquidfuels/plugins/BuildcraftPlugin.java
@@ -1,5 +1,6 @@
 package redgear.liquidfuels.plugins;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 import redgear.core.mod.IPlugin;
@@ -7,8 +8,8 @@ import redgear.core.mod.ModUtils;
 import redgear.core.mod.Mods;
 import redgear.liquidfuels.core.LiquidFuels;
 import buildcraft.api.fuels.IronEngineFuel;
-import buildcraft.api.recipes.RefineryRecipes;
 import cpw.mods.fml.common.LoaderState.ModState;
+import cpw.mods.fml.common.event.FMLInterModComms;
 
 public class BuildcraftPlugin implements IPlugin {
 
@@ -67,28 +68,34 @@ public class BuildcraftPlugin implements IPlugin {
 						+ fuelFromCreosotelRate, fuelFromCreosotelRate);
 
 		if (ethanolToFuelRate > 1 && fuelFromEthanolRate > 1)
-			RefineryRecipes.addRecipe(FluidRegistry.getFluidStack("bioethanol", ethanolToFuelRate),
+			addRefineryRecipe(FluidRegistry.getFluidStack("bioethanol", ethanolToFuelRate),
 					FluidRegistry.getFluidStack("fuel", fuelFromEthanolRate), 12, 1);
 
 		if (fuelFromCreosotelRate > 1 && fuelFromSeedOilRate > 1)
-			RefineryRecipes.addRecipe(FluidRegistry.getFluidStack("seedoil", seedOilToFuelRate),
+			addRefineryRecipe(FluidRegistry.getFluidStack("seedoil", seedOilToFuelRate),
 					FluidRegistry.getFluidStack("fuel", fuelFromSeedOilRate), 12, 1);
 
 		if (creosoteToFuelRate > 1 && fuelFromCreosotelRate > 1)
-			RefineryRecipes.addRecipe(FluidRegistry.getFluidStack("Creosote Oil", creosoteToFuelRate),
+			addRefineryRecipe(FluidRegistry.getFluidStack("Creosote Oil", creosoteToFuelRate),
 					FluidRegistry.getFluidStack("fuel", fuelFromCreosotelRate), 12, 1);
 
-		RefineryRecipes.addRecipe(new FluidStack(LiquidFuels.dieselFluid, 1), FluidRegistry.getFluidStack("fuel", 1),
-				12, 1);
-		RefineryRecipes.addRecipe(new FluidStack(LiquidFuels.gasolineFluid, 1), FluidRegistry.getFluidStack("fuel", 1),
-				12, 1);
-		RefineryRecipes.addRecipe(new FluidStack(LiquidFuels.keroseneFluid, 1), FluidRegistry.getFluidStack("fuel", 1),
-				12, 1);
+		addRefineryRecipe(new FluidStack(LiquidFuels.dieselFluid, 1), FluidRegistry.getFluidStack("fuel", 1), 12, 1);
+		addRefineryRecipe(new FluidStack(LiquidFuels.gasolineFluid, 1), FluidRegistry.getFluidStack("fuel", 1), 12, 1);
+		addRefineryRecipe(new FluidStack(LiquidFuels.keroseneFluid, 1), FluidRegistry.getFluidStack("fuel", 1), 12, 1);
 
 		IronEngineFuel.addFuel(LiquidFuels.keroseneFluid, 4, 47250);
 		IronEngineFuel.addFuel(LiquidFuels.gasolineFluid, 8, 14766);
 		IronEngineFuel.addFuel(LiquidFuels.dieselFluid, 4, 153563);
 
+	}
+
+	private void addRefineryRecipe(FluidStack input, FluidStack output, int energy, int delay) {
+		NBTTagCompound tag = new NBTTagCompound();
+		tag.setTag("input", input.writeToNBT(new NBTTagCompound()));
+		tag.setInteger("energy", energy);
+		tag.setInteger("delay", delay);
+		tag.setTag("output", output.writeToNBT(new NBTTagCompound()));
+		FMLInterModComms.sendMessage(Mods.BCEnergy.getId(), "add-refinery-recipe", tag);
 	}
 
 	@Override
